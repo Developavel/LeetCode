a024078f6d41b129371ef12dafee4c2535c4a836
Pavel Mordovin <106969593+Developavel@users.noreply.github.com>
2025-11-23 23:39:17 +0300

    Time: 41 ms (84.52%) | Memory: 54 MB (41.51%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -1,14 +1,1 @@
-/**
- * @param {number} millis
- * @return {Promise}
- */
-async function sleep(millis) {
-    return new Promise(resolve => {
-        setTimeout(resolve, millis);
-    });
-}
-
-/** 
- * let t = Date.now()
- * sleep(100).then(() => console.log(Date.now() - t)) // ~100
- */
+const sleep = ms => new Promise(r => setTimeout(r, ms));
